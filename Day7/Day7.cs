0091fb5c0da6ee784063f5fa2646e4efee011653
Maria Grebinichenko <grebinichenko.maria@gmail.com>
2024-12-07 12:03:12 +0200

    Day7.2

--- Day7/Day7.cs
+++ Day7/Day7.cs
@@ -5,8 +5,9 @@
         private static string[] _input;
         private static Dictionary<long, List<int>> _rules = [];
 
+        static Day7() => Init();
+
         public static long Part1() {
-            Init();
             long result = 0;
             foreach (var rule in _rules)
             {
@@ -16,17 +17,30 @@
             return result;
         }
 
-        public static int Part2() {
-            //Init();
-            var result = 0;
+        public static long Part2() {
+            long result = 0;
+            foreach (var rule in _rules)
+            {
+                if (IsValidThreeOperators(rule.Key, rule.Value, 0, rule.Value[0]))
+                    result += rule.Key;
+            }
             return result;
         }
 
         private static bool IsValid(long target, List<int> sequence, int i, long result) {
             i++;
-            if ((sequence.Count()) == i) return result == target;
+            if ((sequence.Count) == i) return result == target;
 
             return IsValid(target, sequence, i, result + sequence[i]) || IsValid(target, sequence, i, result * sequence[i]);
+        }
+
+        private static bool IsValidThreeOperators(long target, List<int> sequence, int i, long result) {
+            i++;
+            if ((sequence.Count()) == i) return result == target;
+
+            return IsValidThreeOperators(target, sequence, i, result + sequence[i]) 
+                || IsValidThreeOperators(target, sequence, i, result * sequence[i])
+                || IsValidThreeOperators(target, sequence, i, long.Parse(result.ToString() + sequence[i].ToString()));
         }
 
         private static void Init() {
