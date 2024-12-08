f705ffccb8d437f3c99aeca2fb24b24e44279a31
Maria Grebinichenko <grebinichenko.maria@gmail.com>
2024-12-08 15:38:55 +0200

    day 8.2

--- Day8/Day8.cs
+++ Day8/Day8.cs
@@ -4,11 +4,11 @@
     {
         private static string[] _input;
         private static List<Antenna> _antennaList = [];
+        private static List<(int x, int y)> _antiNodes = [];
         private static int _maxX;
         private static int _maxY;
         private static int _minX;
         private static int _minY;
-        private static List<(int x, int y)> _antiNodes = [];
 
         private const char _empty = '.';
 
@@ -28,7 +28,7 @@
                 }
             }
 
-            return _antiNodes.Count();
+            return _antiNodes.Count;
         }
 
         public static long Part2() {
@@ -40,30 +40,55 @@
                     var items = item.ToList();
                     for (int j = i + 1; j < item.Count(); j++)
                     {
-                        GetAntinodes(items[i], items[j]);
+                        GetReccurentAntinodes(items[i], items[j]);
                     }
                 }
             }
 
-            return _antiNodes.Count();
+            return _antiNodes.Count;
         }
 
         public static void GetReccurentAntinodes(Antenna a1, Antenna a2) {
+            if (!IsCounted(a1.x, a1.y)) _antiNodes.Add((a1.x, a1.y));
+            if (!IsCounted(a2.x, a2.y)) _antiNodes.Add((a2.x, a2.y));
             var modX = a1.x - a2.x;
             var modY = a1.y - a2.y;
-            if (IsValid(a1.x + modX, a1.y + modY)) _antiNodes.Add((a1.x + modX, a1.y + modY));
-            if (IsValid(a2.x - modX, a2.y - modY)) _antiNodes.Add((a2.x - modX, a2.y - modY));
+            var x1 = a1.x + modX;
+            var y1 = a1.y + modY;
+            var x2 = a2.x - modX;
+            var y2 = a2.y - modY;
+            while (IsValid(x1, y1))
+            {
+                if (!IsCounted(x1, y1)) _antiNodes.Add((x1, y1));
+                x1 += modX;
+                y1 += modY;
+            }
+            while (IsValid(x2, y2))
+            {
+                if (!IsCounted(x2, y2)) _antiNodes.Add((x2, y2));
+                x2 -= modX;
+                y2 -= modY;
+            }
+
         }
 
         public static void GetAntinodes(Antenna a1, Antenna a2) {
             var modX = a1.x - a2.x;
             var modY = a1.y - a2.y;
-            if (IsValid(a1.x + modX, a1.y + modY)) _antiNodes.Add((a1.x + modX, a1.y + modY));
-            if (IsValid(a2.x - modX, a2.y - modY)) _antiNodes.Add((a2.x - modX, a2.y - modY));
+            var x1 = a1.x + modX;
+            var y1 = a1.y + modY;
+            var x2 = a2.x - modX;
+            var y2 = a2.y - modY;
+            if (!IsCounted(x1, y1) && IsValid(x1, y1)) _antiNodes.Add((x1, y1));
+            if (!IsCounted(x2, y2) && IsValid(x2, y2)) _antiNodes.Add((x2, y2));
         }
 
         public static bool IsValid(int x, int y) {
-            return !_antiNodes.Any(a => a.x == x && a.y == y) && x >= _minX && x <= _maxX && y >= _minY && y <= _maxY;
+            return x >= _minX && x <= _maxX && y >= _minY && y <= _maxY;
+        }
+
+        public static bool IsCounted(int x, int y) {
+            return _antiNodes.Any(a => a.x == x && a.y == y);
         }
 
         private static void Init() {
@@ -89,7 +114,6 @@
     {
         public int x;
         public int y;
-        //public Direction direction;
         public char frequency;
 
         public Antenna(int y, int x) : this() {
@@ -100,11 +124,7 @@
         public Antenna(int y, int x, char f) : this() {
             this.x = x;
             this.y = y;
-            this.frequency = f;
+            frequency = f;
         }
-
-        //public bool Equals(Antenna obj) {
-        //    return obj.x == this.x && obj.y == this.y && obj.direction == this.direction;
-        //}
     }
 }
